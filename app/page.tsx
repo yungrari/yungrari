bba2182271a0dfb43df43ee041e1c917e9c20427
Young Emo <youngemo@Youngs-MacBook-Pro.local>
2025-02-08 18:27:25 +0100

    Add log

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <Blur />
 
-      <div className="flex flex-col font-[family-name:var(--font-geist-sans)] max-w-[540px] mx-auto px-4 [padding:clamp(24px,calc((100vw-540px)/2),72px)_0]">
+      <div className="flex flex-col font-[family-name:var(--font-geist-sans)] max-w-[540px] mx-auto px-6 [padding:clamp(24px,calc((100vw-540px)/2),72px)_0]">
         <header className="grid grid-cols-[92px_1fr] gap-x-4 mb-9">
           <div className="row-[1/3] size-[92px] rounded-full bg-radial-[at_25%_25%] from-white to-neutral-400 to-75%" />
           <h1 className="col-[2/3] self-end text-[18px]">Dan Martynov</h1>
--- ui/footer.tsx
+++ ui/footer.tsx
@@ -9,7 +9,7 @@ export default async function Footer() {
 
   return (
     <footer className="font-[family-name:var(--font-geist-mono)] uppercase border-t-1 border-neutral-100 text-[11px] text-neutral-400 mt-16">
-      <div className="max-w-[540px] mx-auto p-4 flex justify-between">
+      <div className="max-w-[540px] mx-auto px-6 py-4 flex justify-between">
         <City city={headersList.get('x-geo-city') || ''} />
         <Weather latitude={headersList.get('x-geo-latitude') || ''} longitude={headersList.get('x-geo-longitude') || ''} />
         <Time time={new Date().toLocaleTimeString('en-US', { hour12: false })} />
