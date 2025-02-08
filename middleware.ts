e608bc30d75399cf8c360381a4f1e7a9aea98bda
Young Emo <youngemo@Youngs-MacBook-Pro.local>
2025-02-08 17:20:03 +0100

    Add log

--- middleware.ts
+++ middleware.ts
@@ -3,16 +3,22 @@ import type { NextRequest } from 'next/server'
 import { geolocation } from '@vercel/functions'
  
 export function middleware(request: NextRequest) {
-  const geo = geolocation(request)
   const response = NextResponse.next()
+  const geo = geolocation(request)
 
-  console.log({geo});
+  console.log('geo', geo)
 
-  Object.entries(geo).forEach(([name, value]) => {
-    if (value) {
-      response.headers.set(`x-${name}`, value)
-    }
-  })
+  if (geo.city) {
+    response.headers.set('x-geo-city', geo.city)
+  }
+
+  if (geo.latitude) {
+    response.headers.set('x-geo-latitude', geo.latitude)
+  }
+
+  if (geo.longitude) {
+    response.headers.set('x-geo-longitude', geo.longitude)
+  }
 
   return response
 }
