3c95d50e52608934d733e5dae1213283cdb6d0e3
Young Emo <youngemo@Youngs-MacBook-Pro.local>
2025-02-08 17:59:44 +0100

    Add log

--- ui/footer.tsx
+++ ui/footer.tsx
@@ -8,11 +8,11 @@ export default async function Footer() {
   const headersList = await headers()
 
   return (
-    <footer className="font-[family-name:var(--font-geist-mono)] border-t-1 border-neutral-100 text-[11px] text-neutral-400 py-4 mt-16">
+    <footer className="font-[family-name:var(--font-geist-mono)] uppercase border-t-1 border-neutral-100 text-[11px] text-neutral-400 py-4 mt-16">
       <div className="max-w-[540px] mx-auto flex justify-between">
         <City city={headersList.get('x-geo-city') || ''} />
         <Weather latitude={headersList.get('x-geo-latitude') || ''} longitude={headersList.get('x-geo-longitude') || ''} />
-        <Time />
+        <Time time={new Date().toLocaleTimeString('en-US', { hour12: false })} />
       </div>
     </footer>
   )
